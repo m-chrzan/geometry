2b531e48514af05678a22182f078de3888401800
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-11-11 14:21:32 +0100

    Test Rectangle copy constructor

--- rectangle_test.cc
+++ rectangle_test.cc
@@ -9,6 +9,11 @@ void testRectangleConstructors() {
     checkEqual<long long>(r.height(), 5, "Set height correctly.");
     checkEqual<Position>(r.pos(), Position(-1, 3), "Set position correctly.");
 
+    Rectangle s(r);
+    checkEqual<long long>(s.width(), 2, "Set width correctly.");
+    checkEqual<long long>(s.height(), 5, "Set height correctly.");
+    checkEqual<Position>(s.pos(), Position(-1, 3), "Set position correctly.");
+
     finishTest();
 }
 
